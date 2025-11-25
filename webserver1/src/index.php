31a368082716d7b83b4a18046ea7a8d6827fe4d2
David Perez <david.perez.dam2@gmail.com>
2025-11-25 13:53:40 +0100

    php server

---
+++ webserver1/src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Hello World</title>
+</head>
+
+<body>
+  <h1>
+    <?php
+    echo "<h1>Hello World from PHP running in Docker!</h1>";
+    ?>
+
+  </h1>
+</body>
+
+</html>
